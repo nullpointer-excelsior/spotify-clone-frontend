b8fa5efa2be344787a1250d02a4a4712b38e90ae
nullpointer-excelsior <red.tiger.developer@proton.me>
2024-01-06 14:07:36 -0300

    ssr node enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
-  adapter: vercel()
+  adapter: node({
+    mode:  'standalone'
+  }),
+  vite: {
+    build: {
+        rollupOptions: {
+            external: ['sharp'],
+        }
+    }
+}
 });
